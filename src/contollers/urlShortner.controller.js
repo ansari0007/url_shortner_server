d65c80d786522f70bcc70708a12aa2504f98e4df
Najm <nazmuddin@gamerpe.in>
2024-07-05 00:38:22 +0530

    added base ulr

--- src/contollers/urlShortner.controller.js
+++ src/contollers/urlShortner.controller.js
@@ -14,7 +14,11 @@ const urlShortner=async(req,res)=>{
         const hash = nanoid(8);
         const shortUrl = new urlShortnerModel({ originalUrl, hash });
         await shortUrl.save();
-        res.json({ shortUrl: `http://localhost:8000/${hash}` });
+        // url-shortner-server-xi.vercel.app
+        const baseURL=process.env.BASE_URL
+        const data={shortUrl: `${baseURL}/${hash}` }
+        return res.status(200).json({ status: 201, message: 'short url generated successfully',data:data });          
+
 }
 
 const url= async (req, res) => {
